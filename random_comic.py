857d7f7ff650967b1cca905e120d7846ac9ac989
Aprajita <aprajita.a.dev@gmail.com>
2022-10-12 21:50:41 +0200

    Update random_comic.py
    Adding votes

--- random_comic.py
+++ random_comic.py
@@ -1,8 +1,41 @@
 import requests
 import json
 import random
+from flask import Flask, render_template , request
+
+app = Flask(__name__)
+# line 6 turns  this into a flask framework application 
 
 
+VOTED = {}
+
+@app.route('/vote/<int:id>/<string:vote>')
+def comic_vote(id,vote):
+    # id = requests.args.get["id"]
+    # vote = requests.args.get["vote"]
+    if vote != "good" or vote!= "bad":
+        raise ValueError("Invalid input")
+    if id in VOTED:
+        if vote == "good":
+            VOTED[id]["good"] +=1
+        elif vote == "bad":
+            VOTED[id]["bad"] +=1
+    else:
+        if vote == "good":
+            VOTED[id]= {
+                "good":1,
+                "bad":0
+            }
+        elif vote == "bad":
+            VOTED[id] = {
+                "good":0,
+                "bad":1
+            }
+        
+    return VOTED[id]
+    
+
+@app.route("/")
 def get_random_comic():
     response = requests.get("https://xkcd.com/info.0.json")
     receive = response.content
@@ -14,5 +47,10 @@ def get_random_comic():
     new_value = json.loads(new_receive.decode('utf-8'))
     return new_value
     
-    
-get_random_comic()
+
+# print(get_random_comic())
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
